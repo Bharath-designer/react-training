94be82c51610b747196fbcd2c489e7c55059333c
bharath-designer <bharathv.developer@gmail.com>
2024-09-23 15:55:16 +0530

    build added

--- 05. farmistclone/vite.config.js
+++ 05. farmistclone/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:'/react-training/05.%20boardclone/dist/'
+  base:'/react-training/05.%20farmistclone/dist/'
 })
